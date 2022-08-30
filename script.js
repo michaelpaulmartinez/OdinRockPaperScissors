a6b917d34e1b85c165a272656c3521347bd2f349
Michael <michaelpaulmartinez@gmail.com>
2022-08-30 14:24:48 -0700

    Added Dom Display, improvements to be made...
    display all answers, add text entry for answers.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log("its ya boi boyoyoyoyo");
+
 
     let gameArray =  [
         "Rock", "Paper", "Scissors"
@@ -8,6 +8,13 @@ console.log("its ya boi boyoyoyoyo");
     let userInput;
     let userScore = 0;
     let computerScore = 0;
+    const playerDisplay = document.querySelector("#playerDisplay");
+    const compDisplay = document.querySelector("#compDisplay");
+    const buttonDisplay = document.querySelector("#playButton");
+    let winningScore = 3;
+    let isGameOver = false;
+    const resultDisplay = document.querySelector("#resultDisplay");
+    const finalDisplay = document.querySelector("#finalDisplay");
 
 
 function getComputerChoice() {
@@ -28,31 +35,74 @@ function getComputerChoice() {
         if(playerFinal == "rock" && computerFinal == "rock" ||
         playerFinal == "scissors" && computerFinal == "scissors" ||
         playerFinal == "paper" && computerFinal == "paper") {
-            console.log(`Draw, ${playerFinal} equals ${computerFinal}`);
+            resultDisplay.textContent = `Draw, ${playerFinal} equals ${computerFinal}`;
+            
             return;
         }
         else if (playerFinal == "rock" && computerFinal == "scissors" ||
         playerFinal == "scissors" && computerFinal == "paper" ||
         playerFinal == "paper" && computerFinal == "rock") {
             userScore = userScore + 1;
-            console.log(`You win, ${playerFinal} beats ${computerFinal}`);
+            resultDisplay.textContent = `You win, ${playerFinal} beats ${computerFinal}`;
+            
             return;
         }
         else if (playerFinal == "rock" && computerFinal == "paper" ||
         playerFinal == "scissors" && computerFinal == "rock" ||
         playerFinal == "paper" && computerFinal == "scissors") {
             computerScore = computerScore + 1;
-            console.log(`You lose, ${computerFinal} beats ${playerFinal}`);
+            resultDisplay.textContent = `You lose, ${computerFinal} beats ${playerFinal}`;
+            
             return;
         }
     }
 
-        function game() {
+    buttonDisplay.addEventListener('click', function() {
+        
+        if (!isGameOver) {
+            
+            playRound();
+        playerDisplay.textContent = userScore;
+        compDisplay.textContent = computerScore;
+        if (userScore == winningScore &&  computerScore == winningScore) {
+            finalDisplay.textContent = `Draw, ${computerScore} to ${userScore}`;
+            isGameOver = true;
+            buttonDisplay.innerText = "New Game";
+            return;
+        }
+        else if (computerScore == winningScore) {
+            finalDisplay.textContent = `You lose, ${computerScore} to ${userScore}`;
+            isGameOver = true;
+            buttonDisplay.innerText = "New Game";
+            return;
+        }
+        
+        else if (userScore == winningScore) {
+            finalDisplay.textContent = `You win, ${userScore} to ${computerScore}`;
+            isGameOver = true;
+            buttonDisplay.innerText = "New Game";
+            return;
+        }
+        }
+        else if(isGameOver) {
+            buttonDisplay.innerText = "Play Round";
+            isGameOver = false;
             userScore = 0;
             computerScore = 0;
-            for (let i = 0; i < 5; i++) {
-                playRound();
-            }
+            playRound();
+            playerDisplay.textContent = userScore;
+        compDisplay.textContent = computerScore;
+        finalDisplay.textContent = "";
+        }
+        
+
+    })
+    
+
+       /* function game() {
+            userScore = 0;
+            computerScore = 0;
+           
             if (userScore > computerScore) {
                 console.log(`You win, ${userScore} to ${computerScore}`);
                 return;
@@ -67,6 +117,6 @@ function getComputerChoice() {
             }
         }
 
-
+*/
 
 
